9a9265e397a2cf5c4ef8a5482521ab66f708aaa0
work-Anthony <shingkwong@192-168-1-103.tpgi.com.au>
2021-05-16 20:57:25 +0930

    Update test-note_compare.R

--- tests/testthat/test-note_compare.R
+++ tests/testthat/test-note_compare.R
@@ -1,10 +1,66 @@
 test_that("note_compare works",{
   
+  #test with one sound file only----
+  
+  #match found test, 1st note match
   note = tibble::tibble(start = 0.35, end = 0.49, sound.files = "JS001.wav", pos = 2)
   
   unit_table = tibble::tibble(start = c(0.30, 0.55, 1.5), end = c(0.51, 0.7, 2.2), 
                       sound.files = "JS001.wav", pos =c(2,3,4))
+  ans = tibble::tibble(sound_file = "JS001.wav", 
+                       becky_note_pos = 2, 
+                       anthony_note_pos = 2,
+                       difference = (0.35-0.3)^2 + (0.49-0.51)^2,
+                       matched = T)
   
-  note_compare(note, unit_table)
+  output = note_compare(note, unit_table)
+  expect_equal(ans,output)
   
+  #no match test
+  note = tibble::tibble(start = 0.1, end = 0.2, sound.files = "JS001.wav", pos = 2)
+  
+  unit_table = tibble::tibble(start = c(0.30, 0.55, 1.5), end = c(0.51, 0.7, 2.2), 
+                              sound.files = "JS001.wav", pos =c(2,3,4))
+  ans = tibble::tibble(sound_file = "JS001.wav", 
+                       becky_note_pos = 2, 
+                       anthony_note_pos = NA,
+                       difference = (0.2-0.1)^2,
+                       matched = F)
+  
+  output = note_compare(note, unit_table)
+  expect_equal(ans,output)
+  
+  #match found test, 3rd note match
+  
+  note = tibble::tibble(start = 1.4, end = 2.3, sound.files = "JS001.wav", pos = 2)
+  
+  unit_table = tibble::tibble(start = c(0.30, 0.55, 1.5, 2.5), end = c(0.51, 0.7, 2.2, 3), 
+                              sound.files = "JS001.wav", pos =c(2,3,4,5))
+  ans = tibble::tibble(sound_file = "JS001.wav", 
+                       becky_note_pos = 2, 
+                       anthony_note_pos = 4,
+                       difference = (1.4-1.5)^2 + (2.3-2.2)^2,
+                       matched = T)
+  
+  output = note_compare(note, unit_table)
+  expect_equal(ans,output)
+  
+  #multiple sound files test
+  
+  note = tibble::tibble(start = 0.6, end = 0.7, sound.files = "JS002.wav", pos = 1)
+  
+  unit_table1 = tibble::tibble(start = c(0.30, 0.55, 1.5, 2.5), end = c(0.51, 0.7, 2.2, 3), 
+                              sound.files = "JS001.wav", pos =c(1,2,3,4))
+  unit_table2 = tibble::tibble(start = c(0.2, 0.4, 0.55, 2.5), end = c(0.35, 0.5, 0.8, 3), 
+                              sound.files = "JS002.wav", pos =c(1,2,3,4))
+  unit_table = rbind(unit_table1, unit_table2)
+  
+  ans = tibble::tibble(sound_file = "JS002.wav", 
+                       becky_note_pos = 1, 
+                       anthony_note_pos = 3,
+                       difference = (0.6-0.55)^2 + (0.7-0.8)^2,
+                       matched = T)
+  
+  output = note_compare(note, unit_table)
+  expect_equal(ans,output)
 })
