64b63c41e83e0d5f518c2302014380a2195dd797
deponent-verb <deponentverb@hotmail.com>
2021-05-26 22:30:48 +0930

    Create add_transitions.R

---
+++ R/add_transitions.R
@@ -0,0 +1,45 @@
+#' add_transitions function
+#' 
+#' Takes in a unit table of notes and adds a new column
+#' indicating the next adjacent note class transition. 
+#' For example, if note A is immediately followed by note B, 
+#' note A is recorded as having a A-B transition. The notes
+#' from each recording are treated separately. 
+#'
+#' @param unit_table: A unit table tibble containing the original sound file, 
+#' start/end times of the note,the note position and note_label. 
+#'
+#' @return
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter
+#' 
+#' @export
+#'
+#' @examples unit_table = tibble(start = c(0.37, 0.6, 0.2, 1.8), end = c(0.45, 0.7, 0.4, 2), pos = c(1,2,1,2),sound.files = c("JS001.wav","JS001.wav","JS002.wav","JS002.wav") , note_label = "Curve")
+#' add_transitions(unit_table)
+add_transitions <- function(unit_table){
+  
+  #check inputs
+  
+  #check all the correct columns are present
+  correct_cols = c("start", "end", "sound.files","note_label", "pos")
+  if(sum(colnames(unit_table) %in% correct_cols ) != length(correct_cols) ){
+    stop("note argument does not have the correct columns. See documentation.")
+  }
+  
+  #function starts here
+  
+  #obtain list of songs
+  song_list = unit_table$sound.files %>% unique()
+  
+  #loop over each song
+  for(song in song_list){
+    print(song)
+  }
+  
+ song_table = unit_table %>%
+   dplyr::filter(sound.files == "")
+  
+  return(0)
+}
