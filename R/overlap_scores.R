fda192d808c2b749bd8dca87457d1e3dcf222b64
deponent-verb <deponentverb@hotmail.com>
2021-07-03 19:23:45 +0930

    Create overlap_scores.R

---
+++ R/overlap_scores.R
@@ -0,0 +1,15 @@
+#' overlap_scores function
+#' 
+#' Compares two unit tables by computing the overlap scores for each pair
+#' of matched notes. 
+#'
+#' @param table1 
+#' @param table2 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+overlap_scores = function(table1, table2){
+  
+}
