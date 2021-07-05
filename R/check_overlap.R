952b98903a5897532f499cab24eb0859dcefb59c
work-Anthony <shingkwong@192-168-1-104.tpgi.com.au>
2021-07-05 13:45:01 +0930

    update

---
+++ R/check_overlap.R
@@ -0,0 +1,14 @@
+#' check_overlap function
+#' 
+#' Takes a note (interval with start and end point) and matches 
+#'
+#' @param table 
+#' @param note 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+check_overlap = function(table, note){
+  
+}
--- R/overlap_scores.R
+++ R/overlap_scores.R
@@ -1,7 +1,12 @@
 #' overlap_scores function
 #' 
 #' Compares two unit tables by computing the overlap scores for each pair
-#' of matched notes. 
+#' of matched notes. Notes are considered matching if their intervals overlap. 
+#' 3 scores are computed for each note pair. For any matched pair, denote
+#' the intervals as the sets y and z. The scores are defined as follows.
+#' \eqn{score1 = (y \cap z) / y},
+#' \eqn{score2 = (y \cap z) / (y \cup z)},
+#' \eqn{score3 = |(y \cap z)|^2 / |y| \times |z|}
 #'
 #' @param table1 
 #' @param table2 
@@ -9,7 +14,14 @@
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples asdad
 overlap_scores = function(table1, table2){
   
 }
+
+note1 = c(0.1,0.7)
+note2 = c(0.5,0.9)
+
+#case of 1 overlap
+
+#case of multiple overlaps
