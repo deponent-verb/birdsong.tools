08438837b85d765ab38afbd948f8823dc0901df5
work-Anthony <shingkwong@192-168-1-110.tpgi.com.au>
2021-07-14 22:46:18 +0930

    Create test-compute_oscores.R

---
+++ tests/testthat/test-compute_oscores.R
@@ -0,0 +1,26 @@
+test_that("compute_oscores",{
+  table1 = tibble::tibble(start = c(0.30, 0.55, 1.5, 2.5), end = c(0.51, 0.7, 2.2, 3), 
+                          sound.files = c(rep("JS001.wav",2),rep("JS002.wav",2)) , pos =c(1,2,3,4), note_label = "Curve")
+  table2 = tibble::tibble(start = c(0.25, 0.65, 1.6, 2.4), end = c(0.45, 0.75, 2.1, 2.9), 
+                          sound.files = c(rep("JS001.wav",2),rep("JS002.wav",2)), pos =c(1,2,3,4), note_label = "Unclassifed")
+  
+  output = compute_oscores(table1,table2)
+  
+  #compute answer
+  truth_tables = list(
+    table1[1:2,],
+    table1[3:4,]
+  )
+  comp_tables = list(
+    table2[1:2,],
+    table2[3:4,]
+  )
+  
+  table_scores = list()
+  for(i in 1:2){
+    table_scores[[i]] = compute_oscores_dep2(truth_tables[[i]],comp_tables[[i]])
+  }
+  score_table = do.call(rbind, table_scores)
+  ans = colSums(score_table)
+  expect_equal(output,ans)
+})
